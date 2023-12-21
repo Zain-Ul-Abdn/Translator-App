8c76507deea8fe4eea61d2dffaa06919c3485b2f
Zain_@Developer <zainmehboob376@gmail.com>
2023-12-22 02:11:42 +0500

    Splash Screen design completed

--- app/src/main/java/com/example/translationapp/MainActivity.java
+++ app/src/main/java/com/example/translationapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.translationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Removing default top header of application
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); //This line hide default header
+
+
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
